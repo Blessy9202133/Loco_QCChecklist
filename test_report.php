a856e868ca7813b9d6148227d70417acc5b5e3ba
Sushma <sushmaviasblessy@gmail.com>
2026-07-21 10:18:13 +0530

    added swr

--- test_report.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_POST['loco-id'] = '100';
-$_POST['railway-division'] = 'SER';
-$_POST['shed-name'] = 'Deen Dayal Upadhyay (DDUE)';
-$_SERVER['REQUEST_METHOD'] = 'POST';
-include 'generateReport.php';
-?>
